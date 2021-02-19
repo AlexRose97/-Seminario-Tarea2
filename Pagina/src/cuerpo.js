30855b9b96cd428ac9599fe7d9a93e4d19cf4737
Alex <aexlopez@gmail.com>
2021-02-18 21:29:05 -0600

    pagina completa

--- Pagina/src/cuerpo.js
+++ Pagina/src/cuerpo.js
@@ -5,22 +5,50 @@ import Paper from "@material-ui/core/Paper";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    height: '100vh',
-    justifyContent: 'center',
-    textAlign: 'center',
-    alignItems: 'center'
+    height: "100vh",
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems: "center",
   },
 }));
 
+const PedirCamara = () => {
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+  navigator.getUserMedia(
+    {
+      video: true,
+    },
+    (stream) => {
+      let video = document.getElementsByClassName("app__videoFeed")[0];
+      if (video) {
+        video.srcObject = stream;
+      }
+    },
+    (err) => console.error(err)
+  );
+};
+
 export default function Scuerpo() {
   const classes = useStyles();
-
+  const pedirCam = PedirCamara();
   return (
     <div className={classes.root}>
       <Paper>
         <h1>Alex Rene Lopez Rosa</h1>
-        <h1>201602999</h1> 
+        <h1>201602999</h1>
       </Paper>
+      <video
+        height={500}
+        width={500}
+        muted
+        autoPlay
+        className="app__videoFeed"
+      ></video>
     </div>
   );
 }
